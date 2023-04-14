573ac13ceffbfe087d895b3ee6f94cb53a00d5f0
ChristianTaboada <taboada.christian@gmail.com>
2023-04-14 02:58:12 -0300

    asd

--- api/src/db.js
+++ api/src/db.js
@@ -9,15 +9,15 @@ const {
   DB_DEPLOY,
 } = process.env;
 // Localhost
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+ // logging: false, // set to console.log to see the raw SQL queries
+ // native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//});
+// Railway
+ const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-// Railway
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//  logging: false, // set to console.log to see the raw SQL queries
-//  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+ });
 
 const basename = path.basename(__filename);
 
