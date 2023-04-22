2594a3f02ebe2e993ba6831ca17fb9bbb7b30785
ChristianTaboada <taboada.christian@gmail.com>
2023-04-22 02:52:26 -0300

    update

--- api/src/db.js
+++ api/src/db.js
@@ -6,10 +6,11 @@ const {
   DB_USER,
   DB_PASSWORD, 
   DB_HOST,
-  DB_DEPLOY,
+  DB_PORT,
+  DB_NAME,
 } = process.env;
  //Localhost
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
